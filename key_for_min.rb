3a8e1c99eb512cf2efcad8f52549630fc61da946
Shun Yao <ysmiracle@hotmail.com>
2018-03-29 04:41:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if min == nil || value < min
-      min == value
+      min = value
       return_key = key
     end
   end
